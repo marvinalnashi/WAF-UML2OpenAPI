f41ea91411b251c169a14996f20f303235d8f09a
marvinalnashi <marvinalnashi@hotmail.com>
2024-06-02 22:45:18 +0200

    Added spinner loading animation for when the application starts

--- converter-frontend/UMLtoOpenAPISpec/src/app/app.component.ts
+++ converter-frontend/UMLtoOpenAPISpec/src/app/app.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { GenerationComponent } from './generation/generation.component';
+import {NgxSpinnerComponent, NgxSpinnerService} from "ngx-spinner";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [GenerationComponent],
+  imports: [GenerationComponent, NgxSpinnerComponent],
   templateUrl: './app.component.html',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private spinner: NgxSpinnerService) {
+  }
+
+  ngOnInit() {
+    this.showSpinner();
+  }
+
+  showSpinner(): void {
+    this.spinner.show(undefined, {
+      type: 'ball-spin-clockwise-fade-rotating',
+      size: 'medium',
+      bdColor: 'rgba(0, 0, 0, 0.8)',
+      color: '#fff',
+      fullScreen: true
+    });
+
+    setTimeout(() => {
+      this.spinner.hide();
+    }, 3000);
+  }
 }
