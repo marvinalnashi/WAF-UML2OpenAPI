3fb8448192e47de79ab918e9d28038658645dca0
marvinalnashi <marvinalnashi@hotmail.com>
2024-06-17 13:15:40 +0200

    The previous sessions popup dialog no longer creates a blob for the uploaded UML diagram of a session, as it wasn't being stored correctly
    - The previous session popup dialog now only downloads the generated OpenAPI specification of a previous session. It no longer also downloads an XML file containing the name of the uploaded UML diagram. To retrieve the UML diagrams correctly, an object storage service may need to be used.

--- converter-frontend/UMLtoOpenAPISpec/src/app/session-list-dialog/session-list-dialog.component.ts
+++ converter-frontend/UMLtoOpenAPISpec/src/app/session-list-dialog/session-list-dialog.component.ts
@@ -76,18 +76,8 @@ export class SessionListDialogComponent implements OnInit {
    */
   downloadSession(session: any) {
     this.sessionService.getSession(session.id).subscribe((data) => {
-      const umlBlob = new Blob([data.umlDiagram], { type: 'text/xml' });
       const openApiBlob = new Blob([data.openApiSpec], { type: 'application/json' });
-
-      const umlUrl = window.URL.createObjectURL(umlBlob);
       const openApiUrl = window.URL.createObjectURL(openApiBlob);
-
-      const umlLink = document.createElement('a');
-      umlLink.href = umlUrl;
-      umlLink.download = `uml-diagram-${session.id}.xml`;
-      document.body.appendChild(umlLink);
-      umlLink.click();
-      document.body.removeChild(umlLink);
 
       const openApiLink = document.createElement('a');
       openApiLink.href = openApiUrl;
