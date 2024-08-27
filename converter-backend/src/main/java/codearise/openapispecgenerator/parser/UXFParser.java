b33f82a88df1488f7f5c5ed0ed737ec7b9db6f54
marvinalnashi <marvinalnashi@hotmail.com>
2024-08-27 23:37:00 +0200

    Partially fixed UXFParser relationship handling

--- converter-backend/src/main/java/codearise/openapispecgenerator/parser/UXFParser.java
+++ converter-backend/src/main/java/codearise/openapispecgenerator/parser/UXFParser.java
@@ -5,6 +5,7 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.InputStream;
@@ -17,6 +18,7 @@ import java.util.Map;
  * Implementation of the DiagramParser interface for UXF diagrams.
  */
 public class UXFParser implements DiagramParser {
+
     /**
      * Parses the uploaded UML diagram to extract its classes.
      *
@@ -55,7 +57,60 @@ public class UXFParser implements DiagramParser {
 
     @Override
     public List<Relationship> parseRelationships(InputStream inputStream) throws Exception {
-        return List.of();
+        List<Relationship> relationships = new ArrayList<>();
+        Map<String, String> classCoordinatesMap = new HashMap<>();
+
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+        Document doc = dBuilder.parse(inputStream);
+        doc.getDocumentElement().normalize();
+
+        NodeList nodeList = doc.getElementsByTagName("element");
+
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            Node node = nodeList.item(i);
+            if (node.getNodeType() == Node.ELEMENT_NODE) {
+                Element element = (Element) node;
+                String id = element.getElementsByTagName("id").item(0).getTextContent();
+
+                if ("UMLClass".equals(id)) {
+                    String panelAttributes = element.getElementsByTagName("panel_attributes").item(0).getTextContent();
+                    String className = panelAttributes.split("\\n")[0].replace("Class ", "").trim();
+                    Element coordinatesElement = (Element) element.getElementsByTagName("coordinates").item(0);
+                    String x = coordinatesElement.getElementsByTagName("x").item(0).getTextContent();
+                    String y = coordinatesElement.getElementsByTagName("y").item(0).getTextContent();
+                    String coordinates = x + "," + y;
+                    classCoordinatesMap.put(coordinates, className);
+                }
+            }
+        }
+
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            Node node = nodeList.item(i);
+            if (node.getNodeType() == Node.ELEMENT_NODE) {
+                Element element = (Element) node;
+                String id = element.getElementsByTagName("id").item(0).getTextContent();
+
+                if ("Relation".equals(id)) {
+                    String panelAttributes = element.getElementsByTagName("panel_attributes").item(0).getTextContent();
+                    String[] parts = panelAttributes.split("\\s+", 2);
+                    String relationshipName = parts.length > 1 ? parts[1] : "Association";
+
+                    String additionalAttributes = element.getElementsByTagName("additional_attributes").item(0).getTextContent();
+                    String[] coords = additionalAttributes.split(";");
+
+                    String fromCoordinates = formatCoordinates(coords[0], coords[1]);
+                    String toCoordinates = formatCoordinates(coords[2], coords[3]);
+
+                    String fromClass = classCoordinatesMap.getOrDefault(fromCoordinates, "<<unknown>>");
+                    String toClass = classCoordinatesMap.getOrDefault(toCoordinates, "<<unknown>>");
+
+                    relationships.add(new Relationship(fromClass, toClass, relationshipName));
+                }
+            }
+        }
+
+        return relationships;
     }
 
     /**
@@ -113,5 +168,9 @@ public class UXFParser implements DiagramParser {
                 }
             }
         }
+    }
+
+    private String formatCoordinates(String x, String y) {
+        return x.trim() + "," + y.trim();
     }
 }
